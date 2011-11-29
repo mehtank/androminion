e73c45ef01c479b1c5ac24d7c519ff0232e9fca0
moohtank@gmail.com <moohtank@gmail.com@479f531a-0727-ffee-c1d2-1d776a0eae4d>
2011-11-29 08:25:26 +0000

    Moved comms classes to vdom directory

---
+++ trunk/VDom/src/com/vdom/comms/NewGame.java
@@ -0,0 +1,15 @@
+package com.vdom.comms;
+
+import java.io.Serializable;
+
+public class NewGame implements Serializable {
+	private static final long serialVersionUID = 229362050690595201L;
+
+	public MyCard[] cards = null;
+	public String[] players = null;
+	
+	public NewGame(MyCard[] cards, String[] players) {			
+		this.cards = cards;
+		this.players = players;
+	}
+}
