f3cc03fa65c577ec175219b579334867f577d2a5
moohtank@gmail.com <moohtank@gmail.com@479f531a-0727-ffee-c1d2-1d776a0eae4d>
2012-01-05 00:42:57 +0000

    Combined record and achievements into single tabbed dialog

--- trunk/androminion/src/com/mehtank/androminion/ui/AchievementsView.java
+++ trunk/androminion/src/com/mehtank/androminion/ui/AchievementsView.java
@@ -1,27 +1,22 @@
 package com.mehtank.androminion.ui;
 
-import android.app.AlertDialog;
 import android.content.Context;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.CheckBox;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 
-import com.mehtank.androminion.Androminion;
-import com.mehtank.androminion.R;
+public class AchievementsView extends FrameLayout {
 
-public class AchievementsDialog {
-
-    public AchievementsDialog(Context top) {
-        FrameLayout fv = new FrameLayout (top);
+    public AchievementsView(Context top, Achievements achievements) {
+    	super(top);
+    	
         ScrollView sv = new ScrollView(top);
         sv.setVerticalScrollBarEnabled(true);
         LinearLayout ll = new LinearLayout(top);
         ll.setOrientation(LinearLayout.VERTICAL);
         ll.setBackgroundColor(0x66000000);
-        Achievements achievements = new Achievements((Androminion) top);
         
         for(int i=0; i < Achievements.keys.length; i++) {
             final boolean b = achievements.hasAchieved(Achievements.keys[i]);
@@ -39,15 +34,7 @@ public class AchievementsDialog {
         }
         
         sv.addView(ll);
-        fv.addView(sv);
-        
-        new AlertDialog.Builder(top)
-//            .setIcon(R.drawable.logo)
-            .setTitle(R.string.achievements_menu)
-            .setView(fv)
-            .setPositiveButton(android.R.string.ok, null)
-            .show();        
-
+        addView(sv);
     }
 
 }
