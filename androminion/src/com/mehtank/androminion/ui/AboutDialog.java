71996603f7e1e06372a6fa43834efaa94d5846c2
aurel.rothaermel@gmail.com <aurel.rothaermel@gmail.com@479f531a-0727-ffee-c1d2-1d776a0eae4d>
2012-01-16 23:01:40 +0000

    Fixed  tab view vs. new android versions bug in AboutDialog. Will fix CombinedStatsDialog later.

--- androminion/src/com/mehtank/androminion/ui/AboutDialog.java
+++ androminion/src/com/mehtank/androminion/ui/AboutDialog.java
@@ -2,13 +2,13 @@ package com.mehtank.androminion.ui;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.WindowManager;
-import android.widget.FrameLayout;
-import android.widget.LinearLayout;
 import android.widget.TabHost;
-import android.widget.TabWidget;
 
 import com.mehtank.androminion.R;
+import com.mehtank.androminion.Androminion;
 
 public class AboutDialog {
 
@@ -20,48 +20,26 @@ public class AboutDialog {
 	}
 	
 	private void showDialog(Context top, boolean showNew) {
-    	TabHost th = new TabHost(top);
-    	LinearLayout ll = new LinearLayout(top);
-    	ll.setOrientation(LinearLayout.VERTICAL);
-    	
-    	TabWidget tw = new TabWidget(top);
-    	tw.setId(android.R.id.tabs);
-    	
-    	FrameLayout fl = new FrameLayout(top);
-    	fl.setId(android.R.id.tabcontent);
-    	
-		AboutView about = new AboutView (top);
-		WhatsNewView whatsnew = new WhatsNewView(top);
-		CreditsView credits = new CreditsView(top);
-		
-    	about.setId(R.id.abouttab1);
-    	whatsnew.setId(R.id.abouttab2);
-    	credits.setId(R.id.abouttab3);
-    	
-    	fl.addView(about);
-    	fl.addView(whatsnew);
-    	fl.addView(credits);
-    	
-    	ll.addView(tw);
-    	ll.addView(fl);
-    	
-    	th.addView(ll);
-    	th.setup();
-    	
+        LayoutInflater inflator = ((Androminion)top).getLayoutInflater();
+        View view = inflator.inflate(R.layout.aboutdialog, null);
+
+        TabHost th=(TabHost)view;
+        th.setup();
+
         // create tab 1
         TabHost.TabSpec spec1 = th.newTabSpec("tab1");
         spec1.setIndicator(top.getResources().getString(R.string.about_menu), top.getResources().getDrawable(android.R.drawable.ic_menu_info_details));
-        spec1.setContent(about.getId());
+        spec1.setContent(R.id.abouttab1);
         th.addTab(spec1);
         //create tab2
         TabHost.TabSpec spec2 = th.newTabSpec("tab2");
         spec2.setIndicator(top.getResources().getString(R.string.whatsnew_menu), top.getResources().getDrawable(android.R.drawable.ic_menu_view));
-        spec2.setContent(whatsnew.getId());
+        spec2.setContent(R.id.abouttab2);
         th.addTab(spec2);
         //create tab3
         TabHost.TabSpec spec3 = th.newTabSpec("tab3");
         spec3.setIndicator(top.getResources().getString(R.string.contrib_menu), top.getResources().getDrawable(android.R.drawable.ic_menu_my_calendar));
-        spec3.setContent(credits.getId());
+        spec3.setContent(R.id.abouttab3);
         th.addTab(spec3);
 
         if (showNew) 
