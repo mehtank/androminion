71f39728afe4a61c904eaae882185cbc87ce8a9c
aurel.rothaermel@gmail.com <aurel.rothaermel@gmail.com@479f531a-0727-ffee-c1d2-1d776a0eae4d>
2012-06-09 22:57:42 +0000

    More ui changes: reset button for stats, all activities fullscreen

--- androminion/src/com/mehtank/androminion/ui/CombinedStatsActivity.java
+++ androminion/src/com/mehtank/androminion/ui/CombinedStatsActivity.java
@@ -4,10 +4,15 @@ import com.mehtank.androminion.R;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class CombinedStatsActivity extends FragmentActivity {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.combinedstatsactivity);
 	}
