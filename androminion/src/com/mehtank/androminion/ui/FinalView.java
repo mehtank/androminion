79b8cfe0a08e9b7e247beea81c8aeedc8ff50efa
aurel.rothaermel@gmail.com <aurel.rothaermel@gmail.com@479f531a-0727-ffee-c1d2-1d776a0eae4d>
2012-07-06 09:51:34 +0000

    FinalView to XML

--- androminion/src/com/mehtank/androminion/ui/FinalView.java
+++ androminion/src/com/mehtank/androminion/ui/FinalView.java
@@ -2,60 +2,40 @@ package com.mehtank.androminion.ui;
 
 import android.content.Context;
 import android.graphics.Color;
-import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.mehtank.androminion.R;
-import com.mehtank.androminion.activities.GameActivity;
 
 public class FinalView extends FrameLayout implements OnClickListener {
 
-	GameActivity top;
-	GameTable gt;
+	private GameTable gt;
+	private int[] cardCounts, embargos;
 
-	TextView tv;
-	TextView name;
-	int[] cardCounts, embargos;
-	Button showCards;
+	private TextView name;
+	private Button showCards;
 
 	public FinalView(Context context, GameTable gt, String nameStr, int numTurns, int[] embargos, int numCards, int[] cardCounts, int vp, boolean winner) {
 		super(context);
 
-		this.top = (GameActivity) context;
 		this.gt = gt;
 		this.embargos = embargos;
 		this.cardCounts = cardCounts;
 
-		FrameLayout.LayoutParams p = new FrameLayout.LayoutParams(
-				ViewGroup.LayoutParams.WRAP_CONTENT,
-				ViewGroup.LayoutParams.WRAP_CONTENT,
-				Gravity.LEFT);
-
-		name = new TextView(top);
-		name.setText(top.getString(R.string.final_view_text, nameStr, "" + vp, "" + numTurns, "" + numCards));
-		name.setLayoutParams(p);
+		LayoutInflater.from(context).inflate(R.layout.finalview, this, true);
+		name = (TextView) findViewById(R.id.name);
+		name.setText(context.getString(R.string.final_view_text, nameStr, "" + vp, "" + numTurns, "" + numCards));
 		if (winner) {
 			name.setTextColor(Color.BLACK);
 			name.setBackgroundColor(Color.YELLOW);
 		}
-		addView(name);
-
-		p = new FrameLayout.LayoutParams(
-				ViewGroup.LayoutParams.WRAP_CONTENT,
-				ViewGroup.LayoutParams.WRAP_CONTENT,
-				Gravity.RIGHT);
-
-		showCards = new Button(top);
-		showCards.setLayoutParams(p);
-		showCards.setText(top.getString(R.string.final_view_card_counts));
-
+		
+		showCards = (Button) findViewById(R.id.showCards);
         showCards.setOnClickListener(this);
-		addView(showCards);
 	}
 
 	@Override
