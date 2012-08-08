ac415403cedb93b1661aa9a8133b2845a511aec6
tkdennis@yahoo.com <tkdennis@yahoo.com@479f531a-0727-ffee-c1d2-1d776a0eae4d>
2012-08-08 23:20:21 +0000

    Fixed Issue 383 Secret Chamber and user confusion

--- trunk/vdom/src/com/vdom/core/Util.java
+++ trunk/vdom/src/com/vdom/core/Util.java
@@ -3,8 +3,15 @@ package com.vdom.core;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import com.vdom.api.Card;
 import com.vdom.api.CardCostComparator;
@@ -274,7 +281,7 @@ public class Util {
         }
         
         Card reactionCard = null;
-        while ((reactionCard = player.getAttackReaction(context, responsible, defended)) != null) {
+        while ((reactionCard = player.getAttackReaction(context, responsible, defended, reactionCard)) != null) {
             GameEvent event = new GameEvent(GameEvent.Type.CardRevealed, context);
             event.card = reactionCard;
             game.broadcastEvent(event);
@@ -479,4 +486,25 @@ public class Util {
 			return ret;
 		}
     }
+
+    public static <K, V extends Comparable<? super V>> Map<K, V> 
+    	sortByValue( Map<K, V> map )
+	{
+	    List<Map.Entry<K, V>> list =
+	        new LinkedList<Map.Entry<K, V>>( map.entrySet() );
+	    Collections.sort( list, new Comparator<Map.Entry<K, V>>()
+	    {
+	        public int compare( Map.Entry<K, V> o1, Map.Entry<K, V> o2 )
+	        {
+	            return (o1.getValue()).compareTo( o2.getValue() );
+	        }
+	    } );
+	
+	    Map<K, V> result = new LinkedHashMap<K, V>();
+	    for (Map.Entry<K, V> entry : list)
+	    {
+	        result.put( entry.getKey(), entry.getValue() );
+	    }
+	    return result;
+	}
 }
--- trunk/vdom/src/com/vdom/players/VDomPlayerChuck.java
+++ trunk/vdom/src/com/vdom/players/VDomPlayerChuck.java
@@ -190,7 +190,7 @@ public class VDomPlayerChuck extends BasePlayer  {
     }
 
 	@Override
-	public Card getAttackReaction(MoveContext context, Card responsible, boolean defended) {
+	public Card getAttackReaction(MoveContext context, Card responsible, boolean defended, Card lastCard) {
 		Card[] reactionCards = getReactionCards(defended);
 		for (Card c : reactionCards) {
 			if (c.equals(Cards.moat) && !reactedMoat) {
