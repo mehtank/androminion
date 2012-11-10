8924de020e3a9412a5f3a395e20950bfc8e30f9b
moohtank@gmail.com <moohtank@gmail.com@479f531a-0727-ffee-c1d2-1d776a0eae4d>
2012-11-10 01:57:26 +0000

    Removing obsolete Dark Ages branch

--- branches/DarkAges/androminion/src/com/mehtank/androminion/util/CheckableEx.java
+++
@@ -1,9 +0,0 @@
-package com.mehtank.androminion.util;
-
-import android.widget.Checkable;
-
-public interface CheckableEx extends Checkable {
-	public void setChecked(boolean arg0, String indicator);
-
-	public void setChecked(boolean arg0, int order, String indicator);
-}
