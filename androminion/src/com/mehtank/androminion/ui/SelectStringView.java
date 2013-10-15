1f19857920e75af9690777c08d3304d702461019
draifen <draifen@479f531a-0727-ffee-c1d2-1d776a0eae4d>
2013-10-15 12:33:08 +0000

    Spacing

--- androminion/src/com/mehtank/androminion/ui/SelectStringView.java
+++ androminion/src/com/mehtank/androminion/ui/SelectStringView.java
@@ -15,43 +15,43 @@ import com.mehtank.androminion.activities.GameActivity;
 import com.vdom.comms.Event;
 
 public class SelectStringView extends BottomInputView implements AdapterView.OnItemClickListener {
-	@SuppressWarnings("unused")
-	private static final String TAG = "SelectStringView";
-	
-	ListView lv;
-        private boolean isNewApi;
-        private List<String> options;
+    @SuppressWarnings("unused")
+    private static final String TAG = "SelectStringView";
 
-	public SelectStringView (GameActivity top, String header, String[] options) {
-            this(top, header, options, false);
-	}
+    ListView lv;
+    private boolean isNewApi;
+    private List<String> options;
 
-	public SelectStringView (GameActivity top, String header, String[] options, boolean isNewApi) {
-		super(top, header);
-                this.isNewApi = isNewApi;
-                this.options = Arrays.asList(options);
-		lv.setAdapter(new ArrayAdapter<String>(top, R.layout.view_selectstring, options));
-	}
+    public SelectStringView (GameActivity top, String header, String[] options) {
+        this(top, header, options, false);
+    }
 
-	@Override
-	public void onItemClick(AdapterView<?> parent, View v, int pos, long id) {
-		if (v instanceof TextView) {
-			((FrameLayout) this.getParent()).removeView(this);
-			String s = (((TextView) v).getText().toString());
-			if (isNewApi) {
-                            top.handle(new Event(Event.EType.OPTION).setInteger(options.indexOf(s)));
-                        } else {
-                            top.handle(new Event(Event.EType.STRING).setString(s));
-                        }
-		}
-	}
+    public SelectStringView (GameActivity top, String header, String[] options, boolean isNewApi) {
+        super(top, header);
+        this.isNewApi = isNewApi;
+        this.options = Arrays.asList(options);
+        lv.setAdapter(new ArrayAdapter<String>(top, R.layout.view_selectstring, options));
+    }
 
-	@Override
-	protected View makeContentView(GameActivity activity) {
-		lv = new ListView(top);
-		lv.setOnItemClickListener(this);
-		lv.setBackgroundDrawable(getResources().getDrawable(R.drawable.bottominputviewborder));
+    @Override
+    public void onItemClick(AdapterView<?> parent, View v, int pos, long id) {
+        if (v instanceof TextView) {
+            ((FrameLayout) this.getParent()).removeView(this);
+            String s = (((TextView) v).getText().toString());
+            if (isNewApi) {
+                top.handle(new Event(Event.EType.OPTION).setInteger(options.indexOf(s)));
+            } else {
+                top.handle(new Event(Event.EType.STRING).setString(s));
+            }
+        }
+    }
 
-		return lv;
-	}
+    @Override
+    protected View makeContentView(GameActivity activity) {
+        lv = new ListView(top);
+        lv.setOnItemClickListener(this);
+        lv.setBackgroundDrawable(getResources().getDrawable(R.drawable.bottominputviewborder));
+
+        return lv;
+    }
 }
