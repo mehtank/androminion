fdc05d6cec85e0dd1cedf4b9195da02e35dc4886
draifen <draifen@479f531a-0727-ffee-c1d2-1d776a0eae4d>
2013-11-05 22:09:20 +0000

    Spacing and formatting

--- androminion/src/com/mehtank/androminion/util/CardGroup.java
+++ androminion/src/com/mehtank/androminion/util/CardGroup.java
@@ -18,120 +18,120 @@ import com.vdom.comms.MyCard;
  *
  */
 public class CardGroup extends BaseAdapter {
-	private static final String TAG = "CardGroup";
-	
-	private Context top;
-	private boolean onTable = false;
-	private ArrayList<CardState> cards = new ArrayList<CardState>();
-	private Comparator<MyCard> cmp = new MyCard.CardCostNameComparator();
-	private boolean sorted = false;
-	
-	// fix bug that lets item countLeft jump around
-	int[] supplySizes = null;
-	int[] embargos = null;
-	
-	public void updateCounts(int[] supplySizes, int[] embargos) {
-		this.supplySizes = supplySizes;
-		this.embargos = embargos;
-		notifyDataSetChanged();
-	}
+    private static final String TAG = "CardGroup";
+
+    private Context top;
+    private boolean onTable = false;
+    private ArrayList<CardState> cards = new ArrayList<CardState>();
+    private Comparator<MyCard> cmp = new MyCard.CardCostNameComparator();
+    private boolean sorted = false;
+
+    // fix bug that lets item countLeft jump around
+    int[] supplySizes = null;
+    int[] embargos = null;
+
+    public void updateCounts(int[] supplySizes, int[] embargos) {
+        this.supplySizes = supplySizes;
+        this.embargos = embargos;
+        notifyDataSetChanged();
+    }
 
 
-	public CardGroup(Context top, boolean onTable) {
-		this.top = top;
-		this.onTable = onTable;
-	}
-	
-	public void addCard(MyCard c) {
-		addCard(c, true);
-	}
+    public CardGroup(Context top, boolean onTable) {
+        this.top = top;
+        this.onTable = onTable;
+    }
 
-	public void addCard(MyCard c, boolean state) {
-		CardState ci = new CardState(c, false, "", -1, !state);
-		ci.onTable = onTable;
-		if (onTable || sorted) { // sort cards that are on the table
-			int i = 0;
-			for (i=0; i < cards.size(); i++) {
-				if(cmp.compare(c, cards.get(i).c) < 0)
-					break;
-			}
-			cards.add(i, ci);
-		} else
-			cards.add(ci);
-		notifyDataSetChanged();
-	}
+    public void addCard(MyCard c) {
+        addCard(c, true);
+    }
 
-	public void updateState(int pos, CardState cs){
-		cards.set(pos, cs);
-		notifyDataSetChanged();
-	}
+    public void addCard(MyCard c, boolean state) {
+        CardState ci = new CardState(c, false, "", -1, !state);
+        ci.onTable = onTable;
+        if (onTable || sorted) { // sort cards that are on the table
+            int i = 0;
+            for (i=0; i < cards.size(); i++) {
+                if(cmp.compare(c, cards.get(i).c) < 0)
+                    break;
+            }
+            cards.add(i, ci);
+        } else
+            cards.add(ci);
+        notifyDataSetChanged();
+    }
 
-	public void removeCard(int pos) {
-		cards.remove(pos);
-		notifyDataSetChanged();
-	}
+    public void updateState(int pos, CardState cs){
+        cards.set(pos, cs);
+        notifyDataSetChanged();
+    }
 
-	public void clear() {
-		cards.clear();
-		notifyDataSetChanged();
-	}
+    public void removeCard(int pos) {
+        cards.remove(pos);
+        notifyDataSetChanged();
+    }
 
-	@Override
-	public int getCount() {
-		return cards.size();
-	}
+    public void clear() {
+        cards.clear();
+        notifyDataSetChanged();
+    }
 
-	@Override
-	public Object getItem(int pos) {
-		return cards.get(pos);
-	}
+    @Override
+    public int getCount() {
+        return cards.size();
+    }
 
-	@Override
-	public long getItemId(int pos) {
-		return 0;
-	}
+    @Override
+    public Object getItem(int pos) {
+        return cards.get(pos);
+    }
 
-	@Override
-	public View getView(int pos, View origView, ViewGroup parent) {
-		CardView cv;
-		CardState cs = cards.get(pos);
-		if(origView == null) {
-			cv = new CardView(top, this, null);
-		} else {
-			cv = (CardView) origView;
-		}
-		cv.setState(cs);
-		try {
-			if (supplySizes != null)
-				cv.setCountLeft(supplySizes[cs.c.id]);
-			if (embargos != null)
-				cv.setEmbargos(embargos[cs.c.id]);
-		} catch (ArrayIndexOutOfBoundsException e) {
-			// TODO See why this is happening?
-			Log.w(TAG, "exception", e);
-		}
+    @Override
+    public long getItemId(int pos) {
+        return 0;
+    }
 
-		return cv;
-	}
-	
-	public void updateCardName(int index, String s, String d) {
-		updateCardName(index, s, d, -1);
-	}
-	public void updateCardName(int index, String s, String d, int c) {
-		for (CardState cs : cards) {
-			if (cs.c.id == index) {
-				cs.c.name = s;
-				cs.c.desc = d;
-				if (c >= 0) cs.c.cost = c;
-			}
-		}
-	}
+    @Override
+    public View getView(int pos, View origView, ViewGroup parent) {
+        CardView cv;
+        CardState cs = cards.get(pos);
+        if(origView == null) {
+            cv = new CardView(top, this, null);
+        } else {
+            cv = (CardView) origView;
+        }
+        cv.setState(cs);
+        try {
+            if (supplySizes != null)
+                cv.setCountLeft(supplySizes[cs.c.id]);
+            if (embargos != null)
+                cv.setEmbargos(embargos[cs.c.id]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            // TODO See why this is happening?
+            Log.w(TAG, "exception", e);
+        }
 
-	public int getPos(int index) {
-		for (int i = 0; i < cards.size(); i++) {
-			if (cards.get(i).c.id == index) 
-				return i;
-		}
-		return -1;
-	}
+        return cv;
+    }
+
+    public void updateCardName(int index, String s, String d) {
+        updateCardName(index, s, d, -1);
+    }
+    public void updateCardName(int index, String s, String d, int c) {
+        for (CardState cs : cards) {
+            if (cs.c.id == index) {
+                cs.c.name = s;
+                cs.c.desc = d;
+                if (c >= 0) cs.c.cost = c;
+            }
+        }
+    }
+
+    public int getPos(int index) {
+        for (int i = 0; i < cards.size(); i++) {
+            if (cards.get(i).c.id == index)
+                return i;
+        }
+        return -1;
+    }
 }
