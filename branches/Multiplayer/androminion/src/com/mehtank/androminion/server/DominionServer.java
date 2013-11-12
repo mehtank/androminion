a35cd9de626d700f398f1ea3d3e758758268ba0a
draifen@gmail.com <draifen@gmail.com@479f531a-0727-ffee-c1d2-1d776a0eae4d>
2013-11-12 13:15:34 +0000

    Spacing

--- branches/Multiplayer/androminion/src/com/mehtank/androminion/server/DominionServer.java
+++ branches/Multiplayer/androminion/src/com/mehtank/androminion/server/DominionServer.java
@@ -13,77 +13,77 @@ import android.os.IBinder;
 import android.util.Log;
 
 public class DominionServer extends Service {
-	@SuppressWarnings("unused")
-	private static final String TAG = "DominionServer";
-	
-	VDomServer vds;
-	private final static String stopped = "Server stopped";
+    @SuppressWarnings("unused")
+    private static final String TAG = "DominionServer";
 
-	@Override
-	public IBinder onBind(Intent intent) {
-		return null;
-	}
-	
-	@Override
-	public void onCreate() {
-		super.onCreate();
-		if (!test().equals(stopped))
-			return;
-		
+    VDomServer vds;
+    private final static String stopped = "Server stopped";
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (!test().equals(stopped))
+            return;
+
         Strings.initContext(this);
-		VDomServer.main(new String[] {
-				"Drew (AI)", "com.vdom.players.VDomPlayerDrew",
-				"Earl (AI)", "com.vdom.players.VDomPlayerEarl",
+        VDomServer.main(new String[] {
+            "Drew (AI)", "com.vdom.players.VDomPlayerDrew",
+                "Earl (AI)", "com.vdom.players.VDomPlayerEarl",
                 "Mary (AI)", "com.vdom.players.VDomPlayerMary",
                 "Chuck (AI)", "com.vdom.players.VDomPlayerChuck",
                 "Sarah (AI)", "com.vdom.players.VDomPlayerSarah",
                 "Patrick (AI)", "com.vdom.players.VDomPlayerPatrick",
-//				"-debug"
-		});
-		vds = VDomServer.me;
-	}
+                //                "-debug"
+        });
+        vds = VDomServer.me;
+    }
 
-	@Override
-	public void onDestroy() {
-		if (vds != null)
-			try {
-				vds.quit();
-			} catch (NullPointerException e) {
-				// whatever.
-			}
-		vds = null;
-		super.onDestroy();
-	}
-	
-	String test() {
-		if (vds == null)
-			return stopped;
-		
-		int port = vds.getPort();
-		if (port == 0)
-			return stopped;
-		
-		String host = getLocalIpAddress();
-		return "Server started on " + (host == null ? "localhost" : host) + ":" + port;
-	}
-	
-	public String getLocalIpAddress() {
-		try {
-			for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); 
-					en.hasMoreElements();) {
-				NetworkInterface intf = en.nextElement();
-				for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); 
-						enumIpAddr.hasMoreElements();) {
-					InetAddress inetAddress = enumIpAddr.nextElement();
-					if (!inetAddress.isLoopbackAddress()) {
-						return inetAddress.getHostAddress().toString();
-					}
-				}
-			}
-		} catch (SocketException ex) {
-			Log.e("DroidServer", ex.toString());
-		}
-		return null;
-	}
-	
+    @Override
+    public void onDestroy() {
+        if (vds != null)
+            try {
+                vds.quit();
+            } catch (NullPointerException e) {
+                // whatever.
+            }
+        vds = null;
+        super.onDestroy();
+    }
+
+    String test() {
+        if (vds == null)
+            return stopped;
+
+        int port = vds.getPort();
+        if (port == 0)
+            return stopped;
+
+        String host = getLocalIpAddress();
+        return "Server started on " + (host == null ? "localhost" : host) + ":" + port;
+    }
+
+    public String getLocalIpAddress() {
+        try {
+            for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces();
+                 en.hasMoreElements();) {
+                NetworkInterface intf = en.nextElement();
+                for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses();
+                     enumIpAddr.hasMoreElements();) {
+                    InetAddress inetAddress = enumIpAddr.nextElement();
+                    if (!inetAddress.isLoopbackAddress()) {
+                        return inetAddress.getHostAddress().toString();
+                    }
+                }
+            }
+        } catch (SocketException ex) {
+            Log.e("DroidServer", ex.toString());
+        }
+        return null;
+    }
+
 }
